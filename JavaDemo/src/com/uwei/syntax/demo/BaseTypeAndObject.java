9dbef190b3d883b4e56137c2e63fc251d623afce
uwei <forwardto9@gmail.com>
2017-11-06 09:25:29 +0800

    add

---
+++ JavaDemo/src/com/uwei/syntax/demo/BaseTypeAndObject.java
@@ -0,0 +1,22 @@
+package com.uwei.syntax.demo;
+
+public class BaseTypeAndObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		Integer intObject = new Integer(i);
+		System.out.print(intObject.toString());
+		
+		byte b = 127; // byte b = 'a';
+		Byte bObject = new Byte(b);
+		System.out.print("\n" + bObject.toString());
+		
+		char c = 'c';
+		Character cObject = new Character(c);
+		System.out.print("\n" + cObject);
+		
+		
+	}
+
+}
