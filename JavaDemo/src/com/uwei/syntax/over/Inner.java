75baa417c2950c653c9a7f9407386c12c2f79e6e
uwei <uweiyuan@tencent.com>
2018-11-14 11:52:13 +0800

    update for inner class and over

---
+++ JavaDemo/src/com/uwei/syntax/over/Inner.java
@@ -0,0 +1,57 @@
+package com.uwei.syntax.over;
+
+
+
+
+
+
+
+
+
+public class Inner {
+	private String name;
+	public Inner() {
+		// TODO Auto-generated constructor stub
+	}
+	public void setName() {
+		Imp imp = new Imp();
+		imp.method();
+	}
+	
+	public Imp impm() {
+		return new Imp();
+	}
+	
+	public PrivateImp impP() {
+		return new PrivateImp();
+	}
+	
+	public class Imp {
+		public void method() {
+			name = "x";
+		}
+		private void info() {
+			System.out.println("Imp info()");
+		}
+	}
+	
+	private class PrivateImp {
+		public void method() {
+			name = "xP";
+		}
+		private void info() {
+			System.out.println("PrivateImp info()");
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Inner inner = new Inner();
+		inner.setName();
+		System.out.println(inner.name);
+		inner.impm().info();
+		inner.impP().info();
+	}
+	
+
+}
---
+++ JavaDemo/src/com/uwei/syntax/over/Over.java
@@ -0,0 +1,59 @@
+package com.uwei.syntax.over;
+import com.uwei.syntax.over.Inner;
+
+
+class InstrumentX {
+	public void play(int x) {
+		System.out.println("InstrumentX.play()");
+	}
+	public void show() {
+		System.out.println("InstrumentX.show()");
+	}
+}
+
+class NoteX {
+	public static final int MIDD = 0, FLAT = 1;
+}
+
+class WindX extends InstrumentX {
+	// 过载(overload)，即有新的实现
+	public void play(NoteX x) {
+		// TODO Auto-generated method stub
+		System.out.println("WindX.play()");
+	}
+	// 覆盖(override)，即包含父类逻辑之后，填充自定义逻辑
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		super.show();
+		System.out.println("WindX.show()");
+	}
+}
+
+
+public class Over {
+
+	public static void tune(InstrumentX i) {
+		i.play(NoteX.FLAT);
+//		i.show();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WindX flute = new WindX();
+//		flute.show();
+		tune(flute);
+		
+		
+		Inner inner = new Inner();
+		inner.setName();
+		
+		// 可见性用于代码框架的控制
+		/*
+		System.out.println(inner.name);
+		inner.impm().info();
+		inner.impP().info();
+		*/
+	}
+
+}
