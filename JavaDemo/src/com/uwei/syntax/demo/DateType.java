7d336cab06383270282c84797d1906e8b2a1def2
uwei <uweiyuan@tencent.com>
2022-03-30 19:45:40 +0800

    update

---
+++ JavaDemo/src/com/uwei/syntax/demo/DateType.java
@@ -0,0 +1,7 @@
+package com.uwei.syntax.demo;
+/**
+author: uwei
+create:2022-03-30
+*/
+public class DateType {
+}
---
+++ JavaDemo/src/com/uwei/syntax/demo/NumberType.java
@@ -0,0 +1,13 @@
+package com.uwei.syntax.demo;
+
+/**
+ * author: uwei
+ * create:2022-03-30
+ */
+public class Number {
+    public static void main(String[] args) {
+        Long l = 10L;
+        Number n = 0.2;
+        System.out.println(n);
+    }
+}
